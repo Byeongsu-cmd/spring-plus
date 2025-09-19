67fdff265718124e6bc911b0cdd9c1ca89c8a771
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-09-19 12:12:10 +0900

    Feat - (Comment-User) N+1 문제 해결

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -9,6 +9,11 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    /**
+     * JOIN FETCH을 활용하여 Comment와 연관된 User의 정보를 함께 조회한다.
+     * 현재 연관관계가 지연 로딩(Lazy)로 설정되어 있어, 조회 시 N+1 문제가 발생할 수 있다.
+     * JOIN FETCH를 통해 조회한 User의 정보가 영속성 컨텍스트 내에 존재하기에 접근 시 추가적인 SELECT 쿼리가 발생하지 않는다.
+     */
+    @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
