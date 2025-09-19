e6589d7b880b4df88d609a459f9e4afbfc3c8604
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-09-19 09:45:08 +0900

    feat - AOP의 이해(@After -> @Before) 변경 및 Debug로 test 완료

--- src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
+++ src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
@@ -4,8 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -18,7 +18,27 @@ public class AdminAccessLoggingAspect {
 
     private final HttpServletRequest request;
 
-    @After("execution(* org.example.expert.domain.user.controller.UserController.getUser(..))")
+    /**
+     * Debug로 실행 후 체크하는 법
+     * 브레이크 포인트 설정 후 메서드 호출
+     * Evaluate 창에서 조회
+     * <p>
+     * joinPoint.getSignature().getName()
+     * result = {"changeUserRole"}
+     * <p>
+     * joinPoint.getArgs()
+     * result ={value = 1, role = "ADMIN"}
+     * <p>
+     * request.getRequestURI()
+     * result = {"/admin/users/1"}
+     * <p>
+     * request.getAttribute("userId")
+     * result = {value = 1}
+     * <p>
+     * 로그 출력 확인 (Console 화면에서 확인)
+     * Admin Access Log - User ID: 1, Request Time: 2025-09-19T09:40:31.999309, Request URL: /admin/users/1, Method: changeUserRole
+     */
+    @Before("execution(* org.example.expert.domain.user.controller.UserAdminController.changeUserRole(..))")
     public void logAfterChangeUserRole(JoinPoint joinPoint) {
         String userId = String.valueOf(request.getAttribute("userId"));
         String requestUrl = request.getRequestURI();
