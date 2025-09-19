36c0e01ac7d74fbb5be495e15853f6efec86a431
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-09-19 11:49:12 +0900

    Feat - (Todo-manager) Cascade.PERSIST 구현 및 테스트 완료

--- src/main/java/org/example/expert/domain/todo/entity/Todo.java
+++ src/main/java/org/example/expert/domain/todo/entity/Todo.java
@@ -30,7 +30,11 @@ public class Todo extends Timestamped {
     @OneToMany(mappedBy = "todo", cascade = CascadeType.REMOVE)
     private List<Comment> comments = new ArrayList<>();
 
-    @OneToMany(mappedBy = "todo")
+    /**
+     * PERSIST 이기에 저장은 전이가 되지만, 삭제는 전이되지 않는다.
+     * 삭제도 하고 싶다면 ALL로 변경하면 된다.
+     */
+    @OneToMany(mappedBy = "todo", cascade = CascadeType.PERSIST)
     private List<Manager> managers = new ArrayList<>();
 
     public Todo(String title, String contents, String weather, User user) {
@@ -38,6 +42,16 @@ public class Todo extends Timestamped {
         this.contents = contents;
         this.weather = weather;
         this.user = user;
+
+        /**
+         * 아래와 같이 작성해도 동작하는 이유는 자바 객체 간의 관계가 메모리에서 먼저 설정되고,
+         * JPA가 이를 영속성 켄텍스트에서 추적하며
+         * Cascade 설정을 통해 연관된 Entity를 함께 저장하기 때문
+         *
+         * 다만, 양방향 관계를 유지하지 않으면 조회 시 문제가 생길 수 있다.
+         * 그러니 연관관계를 동기화하는 코드를 Manager Entity 에 만들어 주는 것도 방법이다.
+         * 예) Manager Entity에 동기화 코드 (편의 메서드) todo.getManagers().add(this);
+         */
         this.managers.add(new Manager(user, this));
     }
 }
